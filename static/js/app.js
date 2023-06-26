655771b74055ef0bc2377af71651835c1c506fe1
BirundhaVaratharajan <127799451+BirundhaVaratharajan@users.noreply.github.com>
2023-06-26 01:36:18 -0500

    updated comment and code

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,13 @@
+
 // getting the samples data url
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
 //Global variable to hold the json data from sample.json 
 var data;
-// fetch the JSON data and console log it 
-    // Use D3 to select the dropdown menu
 
+//
+function dashboardInit(){
+// fetch the JSON data and console log it 
 d3.json(url).then((sampledata)=> {
     console.log(`Data: ${sampledata}`);
     //updating the global variable
@@ -19,34 +21,14 @@ d3.json(url).then((sampledata)=> {
      dropdownMenu.append("option").text(subjectId).property("value", subjectId);
    
  });
+ //Display the dashboard with first default meta data and its chart
  demographicInfo(subjectId[0]);
  Chart(subjectId[0]);
-
-    //call the Dashboard initialitation function
-    // dashboardInit();
     
 });
+}
 
-// Use D3 to select the dropdown menu
-let dropdownMenu = d3.select("#selDataset");
-d3.selectAll("#selDataset").on("change", updateDashBoard);
-
-// function dashboardInit()
-// {
-    
-
-//        let subjectId = data.names;
-//        console.log(`Data: ${data}`);
-//        subjectId.forEach((subjectId) => {
-//         // Append each name as an option to the drop down menu
-//         // This is adding each name to the html file as an option element with value = a name in the names array
-//         dropdownMenu.append("option").text(subjectId).property("value", subjectId);
-      
-//     });
-//     demographicInfo(subjectId[0]);
-//     Chart(subjectId[0]);
-
-// }
+// dashboard update function based on the test Id selected in dropdown menu
 function updateDashBoard(){
     let id = d3.select("#selDataset").property("value");
     console.log("id:", id)
@@ -55,9 +37,11 @@ function updateDashBoard(){
 
         
 }
-
+// function to display the metadata(demographic Information)
 function demographicInfo (id){
     let meta = data.metadata;
+    console.log("metadata:", meta)
+
     let metaData = meta.filter((meta) => meta.id == id);
 
     d3.select("#sample-metadata").html("");
@@ -81,7 +65,7 @@ function Chart (id){
     let otu_ids = sampledata[0].otu_ids;
     let otu_labels =sampledata[0].otu_labels;
 
-    //Trace data for Bar chart
+    //Trace top 10 data (slice function is used to get the top 10)for Bar chart
     let traceBar = [{
         x: sample_values.slice(0,10).reverse(),
         y: otu_ids.slice(0,10).map((otu_id) => `OTU ${otu_id}`).reverse(),
@@ -92,10 +76,10 @@ function Chart (id){
     }];
     // Apply title to the bar chart  layout
     let layout = {
-        title: "top 10 OTUs"
+        title: "Top 10 OTUs"
       };
           
-      //Trace data for Bubble chart
+    //Trace data for Bubble chart
     let traceBubble =[{
         x:otu_ids ,
         y:sample_values ,
@@ -104,7 +88,10 @@ function Chart (id){
         marker: {
             size:sample_values,
             color: otu_ids,
-            colorscale: "greens"}
+            sizemode: 'area',
+            sizemin: 6
+    
+        }
        
     }];
     
@@ -119,4 +106,12 @@ function Chart (id){
      Plotly.newPlot("bubble", traceBubble, layoutBubble);
 
 }
+
+//call the init function to display the default metadta 
+dashboardInit();
+// Use D3 to select the dropdown menu
+let dropdownMenu = d3.select("#selDataset");
+//call the updateDashBoard based on event change in dropdown menu
+d3.selectAll("#selDataset").on("change", updateDashBoard);
+
 
