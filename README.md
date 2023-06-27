08576fa12b621f8c6d78b067621706b3b2c731be
BirundhaVaratharajan <127799451+BirundhaVaratharajan@users.noreply.github.com>
2023-06-27 01:08:44 -0500

    code for guage chart and read me file updated

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # belly-button-challenge
+In this assignment, an interactive dashboard has been build to explore the Belly Button Biodiversity , which catalogs the microbes that colonize human navels
+
+A horizontal bar chart has been created with a dropdown menu to display the top 10 OTUs found in that individual by using 
+sample_values as the values for the bar chart.
+otu_ids as the labels for the bar chart.
+otu_labels as the hovertext for the chart.
+
+bar Cha
+A bubble chart that displays each sample using
+otu_ids for the x values,sample_values for the y values and sample_values for the marker size.
+
+otu_ids for the marker colors.
+otu_labels for the text values.
+
+Bubble Chart
+sample metadata, i.e., an individual's demographic information displayed in the dash board
+
+
+
+
--- static/js/app.js
+++ static/js/app.js
@@ -2,56 +2,65 @@
 // getting the samples data url
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-//Global variable to hold the json data from sample.json 
+//Global variable to hold the json data from samples.json 
 var data;
 
-//
+//Init function to read and display the dashboard with first sample data
 function dashboardInit(){
-// fetch the JSON data and console log it 
-d3.json(url).then((sampledata)=> {
-    console.log(`Data: ${sampledata}`);
-    //updating the global variable
-    data = sampledata;
+// using D3 library reading the samplesJ SON data and console log it 
+    d3.json(url).then((sampledata)=> {
 
-    let subjectId = data.names;
-    console.log(`Data: ${data}`);
-    subjectId.forEach((subjectId) => {
-     // Append each name as an option to the drop down menu
-     // This is adding each name to the html file as an option element with value = a name in the names array
-     dropdownMenu.append("option").text(subjectId).property("value", subjectId);
-   
- });
- //Display the dashboard with first default meta data and its chart
- demographicInfo(subjectId[0]);
- Chart(subjectId[0]);
+        //updating the global variable data
+        data = sampledata;
+
+        //console log the data
+        console.log(`Data: ${data}`);
+
+        //getting the all the id from data 
+        let subjectId = data.names;
+        //dropdown menu to list out the all subjectId to test
+        subjectId.forEach((subjectId) => {
+        // Append each id 
+        dropdownMenu.append("option").text(subjectId).property("value", subjectId);
     
-});
+    });
+    //Display the dashboard with first default meta data and its chart
+    demographicInfo(subjectId[0]);
+    Chart(subjectId[0]);
+        
+    });
 }
 
 // dashboard update function based on the test Id selected in dropdown menu
 function updateDashBoard(){
+    //getting the test ID form the fropdown menu
     let id = d3.select("#selDataset").property("value");
     console.log("id:", id)
-    demographicInfo(id);
     Chart(id);
+    demographicInfo(id);
 
         
 }
 // function to display the metadata(demographic Information)
 function demographicInfo (id){
+    //fetching the all meta data 
     let meta = data.metadata;
-    console.log("metadata:", meta)
 
+    //console log meta data
+    console.log("metadata:", meta)
+    //filter and get the metadata that matches the id
     let metaData = meta.filter((meta) => meta.id == id);
 
+    // clearing the old meta data from the demographic info
     d3.select("#sample-metadata").html("");
 
+    //Display each key-value pair from the metadata JSON object on the page
     let demoInfo = Object.entries(metaData[0]);
     demoInfo.forEach(([key,value]) => {
         d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
     });
 
-    // Log the entries Array
+    // console Log the entries 
     console.log(demoInfo);       
 
 }
@@ -59,11 +68,17 @@ function demographicInfo (id){
 function Chart (id){
     
     let samples = data.samples;
+    let meta = data.metadata;
+
     let sampledata = samples.filter((sample) => sample.id === id);
+    let metaData = meta.filter((meta) => meta.id == id);
+
     
     let sample_values = sampledata[0].sample_values;
     let otu_ids = sampledata[0].otu_ids;
     let otu_labels =sampledata[0].otu_labels;
+    //getting washing frequency from meta data
+    let wfreq = metaData[0].wfreq;
 
     //Trace top 10 data (slice function is used to get the top 10)for Bar chart
     let traceBar = [{
@@ -88,22 +103,46 @@ function Chart (id){
         marker: {
             size:sample_values,
             color: otu_ids,
-            sizemode: 'area',
-            sizemin: 6
-    
+            colorscale: "Rainbow"        
         }
        
     }];
+    console.log("wash freq:",wfreq);
+    let traceGauge = [{
+        domain: { x: [0, 1], y: [0, 1] },
+        value: wfreq,
+        title: { text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week", font: {size: 24}},
+        type: "indicator", 
+        mode: "gauge+number",
+        gauge: {
+            axis: {range: [null, 10],tickwidth:2}, 
+            bar: {color: "rgb(68,166,198)"},
+            steps: [
+                { range: [0, 2], color: "yellow" },
+                { range: [2, 4], color: "lime" },
+                { range: [4, 6], color: "green" },
+                { range: [6, 8], color: "cyan" },
+                { range: [8, 10], color: "blue" },
+
+            ]
+        }
+    }];
+
     
+
     // Apply title to the bubble layout
     let layoutBubble = {
        xaxis:{title: " OTU ID"}
       };
-      
-  // Using Plotly to plot the data in a Bar and  bubble chart
+     
+    let layoutGauge = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+
+  // Using Plotly to plot the data in a Bar,  bubble and gauge chart
 
      Plotly.newPlot("bar", traceBar, layout);
      Plotly.newPlot("bubble", traceBubble, layoutBubble);
+     Plotly.newPlot("gauge", traceGauge,layoutGauge);
+
 
 }
 
